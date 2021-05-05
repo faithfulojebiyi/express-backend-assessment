143d202afa3cb3e3c9a90efa90d72d3bccdb1756
faithfulojebiyi <faithfulojebiyi@gmail.com>
2021-05-05 07:16:34 +0100

    Abstracted service functions into utils helpers

--- services/parking.service.js
+++ services/parking.service.js
@@ -1,5 +1,12 @@
 const path = require("path");
 const fs = require("fs");
+const {
+  isExisting,
+  isCarExisting,
+  genRandomNum,
+  checkCarData,
+  removeCar,
+} = require("../utils/helpers");
 
 const lotSize = parseInt(process.env.LOTSIZE);
 
@@ -14,55 +21,6 @@ let exists = false;
 
 const carData = JSON.parse(JSON.stringify(JSON.parse(data), null, 2));
 
-// console.log(carData);
-// console.log(carData.length === lotSize);
-// fs.writeFileSync(
-//   path.resolve(path.resolve(__dirname, "../cardata.json")),
-//   JSON.stringify(farmers, null, 2)
-// );
-
-function isExisting(array, slot) {
-  let slotTaken = false;
-  slotTaken = !!array.find((car) => {
-    return car.slot === slot;
-  });
-  return slotTaken;
-}
-
-function isCarExisting(array, carnumber) {
-  let slotTaken = false;
-  slotTaken = !!array.find((car) => {
-    return car.carNumber === carnumber;
-  });
-  return slotTaken;
-}
-
-function genRandomNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function checkCarData(array, query) {
-  return array.filter((d) => d.slot === query || d.carNumber === query);
-}
-
-// function removeCar(array, key, value) {
-//   const index = array.findIndex((obj) => obj[key] === value);
-//   return index >= 0
-//     ? [...array.slice(0, index), ...array.slice(index + 1)]
-//     : array;
-// }
-
-function removeCar(array, value) {
-  const data = array;
-  const removeindex = data
-    .map(function (item) {
-      return item.slot;
-    })
-    .indexOf(value);
-  data.splice(removeindex, 1);
-  return data;
-}
-
 const parkCar = async (carnumber) => {
   console.log(carData.length);
   if (isCarExisting(carData, carnumber)) {
@@ -70,15 +28,14 @@ const parkCar = async (carnumber) => {
   }
   if (carData.length === 0) {
     a = genRandomNum(minVal, lotSize);
-    const cardata = [];
     const car = {
       slot: a,
       carNumber: carnumber,
     };
-    cardata.push(car);
+    carData.push(car);
     fs.writeFileSync(
       path.resolve(path.resolve(__dirname, "../cardata.json")),
-      JSON.stringify(cardata, null, 2)
+      JSON.stringify(carData, null, 2)
     );
     console.log("is empty");
     return cardata;
@@ -88,9 +45,7 @@ const parkCar = async (carnumber) => {
     do {
       console.log(exists);
       a = genRandomNum(minVal, lotSize);
-      exists = !!carData.find((car) => {
-        return car.slot === a;
-      });
+      exists = isExisting(carData, a);
       console.log(a, exists);
     } while (exists == true);
     const cardata = {
@@ -127,33 +82,3 @@ module.exports = {
   unparkCar,
   getCar,
 };
-// let a = 0;
-// let exists = false;
-// const users = [
-//   {
-//     name: 1,
-//   },
-//   {
-//     name: 2,
-//   },
-// ];
-
-// function genRandomNum(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// if (users.length === 0) {
-//   a = genRandomNum(1, 3);
-//   console.log("is empty");
-// } else {
-//   do {
-//     console.log(exists);
-//     a = genRandomNum(1, 3);
-//     exists = !!users.find((user) => {
-//       return user.name === a;
-//     });
-//     console.log(a, exists);
-//   } while (exists == true);
-// }
-
-// console.log("new", a);
---
+++ utils/helpers.js
@@ -0,0 +1,42 @@
+function isExisting(array, slot) {
+  let slotTaken = false;
+  slotTaken = !!array.find((car) => {
+    return car.slot === slot;
+  });
+  return slotTaken;
+}
+
+function isCarExisting(array, carnumber) {
+  let slotTaken = false;
+  slotTaken = !!array.find((car) => {
+    return car.carNumber === carnumber;
+  });
+  return slotTaken;
+}
+
+function genRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function checkCarData(array, query) {
+  return array.filter((d) => d.slot === query || d.carNumber === query);
+}
+
+function removeCar(array, key) {
+  const data = array;
+  const removeindex = data
+    .map(function (item) {
+      return item.slot;
+    })
+    .indexOf(key);
+  data.splice(removeindex, 1);
+  return data;
+}
+
+module.exports = {
+  isExisting,
+  isCarExisting,
+  genRandomNum,
+  checkCarData,
+  removeCar,
+};
